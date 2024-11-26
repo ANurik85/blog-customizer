1e9d0df9e242c317b87b6acd3129fc04fb7b3267
Nurik <yanzip@yandex.ru>
2024-11-26 15:35:17 +0500

    Исправлен форма сайдбара и компонент размер шрифта

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,13 +2,90 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
+import { Separator } from 'src/ui/separator';
+import { Select } from 'src/ui/select';
+import {
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Text } from 'src/ui/text';
+import { useState } from 'react';
 
-export const ArticleParamsForm = () => {
+interface ArticleParamsFormProps {
+	isOpen: boolean;
+	setIsOpen: (isOpen: boolean) => void;
+	setFontSize: (fontSize: string) => void;
+}
+
+export const ArticleParamsForm = ({
+	isOpen,
+	setIsOpen,
+	setFontSize,
+}: ArticleParamsFormProps) => {
+	const [localFontSize, setLocalFontSize] = useState(fontSizeOptions[0]);
+	const handleArrowClick = () => {
+		setIsOpen(!isOpen);
+	};
+	const handleFontSizeChange = (option: OptionType) => {
+		setLocalFontSize(option);
+	};
+	const handleSubmit = (event: React.FormEvent) => {
+		event.preventDefault();
+		setFontSize(localFontSize.value);
+		setIsOpen(false);
+	};
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={handleArrowClick} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				<form className={styles.form} onSubmit={handleSubmit}>
+					<Text
+						as='div'
+						size={22}
+						weight={800}
+						fontStyle='normal'
+						uppercase={true}
+						align='left'
+						family='open-sans'>
+						Задайте параметры
+					</Text>
+
+					<Select
+						// className={styles.select}
+						selected={fontFamilyOptions[0]}
+						options={fontFamilyOptions}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						name='radio'
+						title='Размер шрифта'
+						options={fontSizeOptions}
+						selected={localFontSize}
+						onChange={handleFontSizeChange}
+					/>
+					<Select
+						selected={fontColors[0]}
+						options={fontColors}
+						title='Цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={backgroundColors[0]}
+						options={backgroundColors}
+						title='Цвет фона'
+					/>
+					<Select
+						selected={contentWidthArr[0]}
+						options={contentWidthArr}
+						title='Ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState /* , useCallback */ } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
@@ -8,24 +8,42 @@ import { defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
+// import { useEffect } from 'react';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [fontSize, setFontSize] = useState(
+		defaultArticleState.fontSizeOption.value
+	);
+
+	const handleOutsideClick = (event: React.MouseEvent<HTMLElement>): void => {
+		const main = document.querySelector(`.${styles.main}`);
+		// console.log('closestContainer:', main);
+		if (main && !main.contains(event.target as Node)) {
+			setIsOpen(true);
+		}
+	};
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
 					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
+					'--font-size': fontSize,
 					'--font-color': defaultArticleState.fontColor.value,
 					'--container-width': defaultArticleState.contentWidth.value,
 					'--bg-color': defaultArticleState.backgroundColor.value,
 				} as CSSProperties
-			}>
-			<ArticleParamsForm />
+			}
+			onClick={handleOutsideClick}>
+			<ArticleParamsForm
+				isOpen={isOpen}
+				setIsOpen={setIsOpen}
+				setFontSize={setFontSize}
+			/>
 			<Article />
 		</main>
 	);
