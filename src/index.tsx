fc966a3a28898440f4503d9eb636fe2d39f386d9
Nurik <yanzip@yandex.ru>
2024-11-27 21:58:29 +0500

    Улучшение index.tsx, перенос ком-та App в App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,57 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState } from 'react';
-import clsx from 'clsx';
-
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
-
-import './styles/index.scss';
-import styles from './styles/index.module.scss';
+import { StrictMode } from 'react';
+import App from './components/app/App';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
-
-const App = () => {
-	const [fontSize, setFontSize] = useState(
-		defaultArticleState.fontSizeOption.value
-	);
-	const [fontFamily, setFontFamily] = useState(
-		defaultArticleState.fontFamilyOption.value
-	);
-	const [fontColor, setFontColor] = useState(
-		defaultArticleState.fontColor.value
-	);
-	const [bgColor, setBgColor] = useState(
-		defaultArticleState.backgroundColor.value
-	);
-	const [contentWidth, setContentWidth] = useState(
-		defaultArticleState.contentWidth.value
-	);
-
-	return (
-		<main
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': fontFamily,
-					'--font-size': fontSize,
-					'--font-color': fontColor,
-					'--container-width': contentWidth,
-					'--bg-color': bgColor,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm
-				setFontSize={setFontSize}
-				setFontFamily={setFontFamily}
-				setFontColor={setFontColor}
-				setBgColor={setBgColor}
-				setContentWidth={setContentWidth}
-			/>
-			<Article />
-		</main>
-	);
-};
 
 root.render(
 	<StrictMode>
